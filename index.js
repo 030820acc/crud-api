2f8364d5c5a9884e9a822d6286f7522e36bf1e07
030820acc <030820acc@gmail.com>
2025-09-09 22:30:36 -0400

    connected db

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const mongoDBString = "mongodb+srv://admin:FYirLB1WRq0HAtv3@backenddb.pndl5u7.mongodb.net/?retryWrites=true&w=majority&appName=backenddb"
 
-app.listen(3000, () => {
-    console.log('server is active on port 3000')
-});
+mongoose.connect(mongoDBString).then(() => {
+    console.log("connected")
+    app.listen(3000, () => {
+        console.log('server is active on port 3000')
+    });
+}).catch(() => {
+    console.log("connection failed")
+})
 
 app.get('/', (req, res) => {
     res.send("hello")
-})
+});
